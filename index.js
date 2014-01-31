34134cd75aac30c2df4a986479aee2e8ca2c7a17
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-01-31 14:45:24 +0100

    Move `takeAsync(fn, onCaller)` and `putAsync(value, fn, onCaller)` inside ManyToManyChannel.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
+'use strict';
+
 var ManyToManyChannel = require('./lib/ManyToManyChannel'),
-    buffers = require('chronic-buffers'),
-    helpers = require('./lib/helpers'),
-    dspatcher = require('./lib/dispatch');
+    buffers = require('chronic-buffers');
 
 module.exports = {
   ManyToManyChannel: ManyToManyChannel,
@@ -14,47 +14,7 @@ function chan(bufOrN) {
     return ManyToManyChannel.create(null);
   }
 
-  var buf = typeof bufOrN === 'number'
-    ? buffers.default(bufOrN)
-    : bufOrN;
+  var buf = typeof bufOrN === 'number' ? buffers.default(bufOrN) : bufOrN;
 
   return ManyToManyChannel.create(buf);
-}
-
-function takeAsync(port, fn, onCaller) {
-  onCaller = (onCaller != null) ? onCaller : true;
-
-  var ret = port.take(helpers.fnHandler(fn));
-
-  if (!ret) {
-    return;
-  }
-
-  if (onCaller) {
-    fn(ret);
-  }
-  else {
-    dispatcher.run(function() {
-      fn(ret);
-    });
-  }
-}
-
-function nop() {}
-
-function putAsync(port, value, fn, onCaller) {
-  fn = (fn != null) ? fn : nop;
-  onCaller = (onCaller != null) ? onCaller : true;
-
-  var ret = port.putAsync(value, helpers.fnHandler(fn));
-
-  if (!ret || fn === nop) {
-    return;
-  }
-
-  if (onCaller) {
-    fn();
-  } else {
-    dispatcher.run(fn);
-  }
 }
