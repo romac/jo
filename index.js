44a257c2ed1573d2771e8568a1db8b1a4c720e69
Romain Ruetschi <romain.ruetschi@gmail.com>
2014-02-05 13:35:47 +0100

    Move chan() to lib/chan.js.

--- index.js
+++ index.js
@@ -2,19 +2,9 @@
 'use strict';
 
 var ManyToManyChannel = require('./lib/ManyToManyChannel'),
-    buffers = require('chronic-buffers');
+    chan = require('./lib/chan');
 
 module.exports = {
   ManyToManyChannel: ManyToManyChannel,
   chan: chan
 };
-
-function chan(bufOrN) {
-  if (!bufOrN) {
-    return ManyToManyChannel.create(null);
-  }
-
-  var buf = typeof bufOrN === 'number' ? buffers.default(bufOrN) : bufOrN;
-
-  return ManyToManyChannel.create(buf);
-}
